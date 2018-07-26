0d545513634739c3812445d2f74936de1420ced3
Keegan Lawley <32738411+keeganl@users.noreply.github.com>
2018-07-26 15:53:18 -0500

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,84 @@
+import speech_recognition as sr
+import os
+import re
+import webbrowser
+import smtplib
+import requests
+from weather import Weather
+
+
+def talkToMe(audio):
+    "speaks audio passed as argument"
+
+    print(audio)
+    for line in audio.splitlines():
+        os.system("say " + audio)
+
+    #  use the system's inbuilt say command instead of mpg123
+    #  text_to_speech = gTTS(text=audio, lang='en')
+    #  text_to_speech.save('audio.mp3')
+    #  os.system('mpg123 audio.mp3')
+
+
+def myCommand():
+    "listens for commands"
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Ready...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio).lower()
+        print('You said: ' + command + '\n')
+
+    #loop back to continue to listen for commands if unrecognizable speech is received
+    except sr.UnknownValueError:
+        talkToMe('What did you say?')
+        command = myCommand()
+
+    return command
+
+
+def assistant(command):
+    "if statements for executing commands"
+
+    if 'open reddit' in command:
+        reg_ex = re.search('open reddit (.*)', command)
+        url = 'https://www.reddit.com/'
+        if reg_ex:
+            subreddit = reg_ex.group(1)
+            url = url + 'r/' + subreddit
+        webbrowser.open(url)
+        print('Done!')
+
+    elif 'music' in command:
+        os.system("open /Applications/Spotify.app")
+        talkToMe("Opening Spotify")
+
+    elif 'open laser control' in command:
+        os.system("open /Applications/laserui.app")
+        talkToMe("done")
+
+    elif 'open website' in command:
+        reg_ex = re.search('open website (.+)', command)
+        if reg_ex:
+            domain = reg_ex.group(1)
+            url = 'https://www.' + domain
+            webbrowser.open(url)
+            print('Done!')
+        else:
+            pass
+
+    else:
+        talkToMe('I don\'t know what you mean!')
+
+
+talkToMe('I am ready for your command') 
+
+#loop to continue executing multiple commands
+while True:
+    assistant(myCommand())
